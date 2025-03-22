a6fc4d6e7ad24e5d4e63981db5c72ddde2af0312
RealED-io <emrealuyo@up.edu.ph>
2025-03-23 02:21:43 +0800

    Updated to Exercise 2.12-2.15

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react'
-import axios from 'axios'
+import personService from './services/persons'
 import Contacts from './components/Contacts'
 import FilterPrompt from './components/FilterPrompt'
 import ContactForm from './components/ContactForm'
@@ -9,21 +9,31 @@ const App = () => {
   const [filter, setFilter] = useState('')
 
   useEffect(() => {
-    axios
-      .get('http://localhost:3001/persons')
-      .then(response => {
-        setPersons(response.data)
-      })
+    personService.getAll().then(p => setPersons(p))
   }, [])
-  
+
   const handleAddContact = (newName, newNumber) => {
     const findPerson = persons.find(p => p.name === newName)
     // Case if person is already at the contacts
     if(findPerson) {
-      alert(`${findPerson.name} is already added to phonebook`)
+      const confirm = window.confirm(`${findPerson.name} is already added to phonebook, replace the old number with a new one?`)
+      if (confirm) {
+        personService.update(findPerson.id, {name: newName, number: newNumber})
+        .then(updated => setPersons(persons.map(p => p.id === updated.id ? updated : p)))
+      } 
     }
+    // Case if person is already at the contacts
     else {
-      setPersons(persons.concat({name: newName, number: newNumber}))
+      personService.create({name: newName, number: newNumber})
+        .then(p => setPersons(persons.concat(p)))
+    }
+  }
+
+  const handleDeleteContact = (id) => {
+    const confirm = window.confirm(`Delete ${persons.find(p => p.id === id).name}`)
+    if(confirm) {
+      personService.remove(id)
+      .then(deleted => setPersons(persons.filter(p => p.id !== deleted.id)))
     }
   }
 
@@ -34,7 +44,7 @@ const App = () => {
       <h2>add a new</h2>
       <ContactForm handleAddContact={handleAddContact}/>
       <h2>Numbers</h2>
-      <Contacts persons={persons} filter={filter}/>
+      <Contacts persons={persons} filter={filter} deletePerson={handleDeleteContact}/>
     </div>
   )
 }
--- part2/phonebook/src/components/Contacts.jsx
+++ part2/phonebook/src/components/Contacts.jsx
@@ -1,9 +1,12 @@
-const Contacts = ({persons, filter}) => {
+const Contacts = ({persons, filter, deletePerson}) => {
     const filteredPersons = persons.filter(p => p.name.toLowerCase().includes(filter.toLowerCase()))
     return(
         <div>
             {filteredPersons.map(person => 
-            <p key={person.name} style={{margin: 0}}> {person.name} {person.number} </p>)}
+            <p key={person.name} style={{margin: 0}}> 
+                {person.name} {person.number} 
+                <button onClick={() => deletePerson(person.id)}>delete</button>
+                </p>)}
         </div>
     )
 }
