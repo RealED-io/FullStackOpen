605278255b8d981adebd4d9b61da9ce773f930e6
RealED-io <emrealuyo@up.edu.ph>
2025-03-22 22:40:46 +0800

    Updated to Exercise 2.11

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,18 +1,21 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 import Contacts from './components/Contacts'
 import FilterPrompt from './components/FilterPrompt'
 import ContactForm from './components/ContactForm'
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ]) 
-  
+  const [persons, setPersons] = useState([]) 
   const [filter, setFilter] = useState('')
 
+  useEffect(() => {
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        setPersons(response.data)
+      })
+  }, [])
+  
   const handleAddContact = (newName, newNumber) => {
     const findPerson = persons.find(p => p.name === newName)
     // Case if person is already at the contacts
