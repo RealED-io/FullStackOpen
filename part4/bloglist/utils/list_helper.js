03102cd6776c7d305feec53ce6cc525253d50b9c
RealED-io <emrealuyo@up.edu.ph>
2025-04-24 06:42:39 +0800

    Updated to Exercise 4.7-Helper Functions Unit Tests

---
+++ part4/bloglist/utils/list_helper.js
@@ -0,0 +1,68 @@
+const dummy = (blogs) => {
+    return 1
+}
+
+
+const totalLikes = (blogs) => {
+    return blogs.reduce((prev, { likes }) => prev + likes, 0)
+}
+
+
+const favoriteBlog = (blogs) => {
+    return blogs.reduce((max, curr) => curr.likes > max.likes ? curr : max)
+}
+
+
+const mostBlogs = (blogs) => {
+    if (!blogs || blogs.length === 0) {
+        return
+    }
+
+    const filteredBlogs = []
+    blogs.forEach(blog => {
+        const existingBlog = filteredBlogs.find(b => b.author === blog.author)
+        if (existingBlog) {
+            existingBlog.blogs++
+        } else {
+            const newBlog = {
+                author: blog.author,
+                blogs: 1
+            }
+            filteredBlogs.push(newBlog)
+        }
+    })
+
+    return filteredBlogs.reduce((max, curr) => curr.blogs > max.blogs ? curr : max)
+}
+
+
+const mostLikes = (blogs) => {
+    if (!blogs || blogs.length === 0) {
+        return
+    }
+
+    const filteredBlogs = []
+    blogs.forEach(blog => {
+        const existingBlog = filteredBlogs.find(b => b.author === blog.author)
+        if (existingBlog) {
+            existingBlog.likes += blog.likes
+        } else {
+            const newBlog = {
+                author: blog.author,
+                likes: blog.likes
+            }
+            filteredBlogs.push(newBlog)
+        }
+    })
+
+    return filteredBlogs.reduce((max, curr) => curr.likes > max.likes ? curr : max)
+}
+
+
+module.exports = {
+    dummy,
+    totalLikes,
+    favoriteBlog,
+    mostBlogs,
+    mostLikes
+}
