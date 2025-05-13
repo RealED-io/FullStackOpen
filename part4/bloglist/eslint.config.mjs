8e83c4143684492ddc9e610b2a744af052ce54a4
RealED-io <emrealuyo@up.edu.ph>
2025-05-14 00:08:05 +0800

    refactored to comply with eslint

--- part4/bloglist/eslint.config.mjs
+++ part4/bloglist/eslint.config.mjs
@@ -1,24 +1,38 @@
-import js from "@eslint/js";
-import globals from "globals";
-import { defineConfig } from "eslint/config";
-import stylisticJs from "@stylistic/eslint-plugin-js"
+import globals from 'globals';
+import js from '@eslint/js';
+import { defineConfig } from 'eslint/config';
+import stylisticJs from '@stylistic/eslint-plugin-js';
 
 
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"] },
+  js.configs.recommended,
   {
-    files: ["**/*.js"],
-    languageOptions: { sourceType: "commonjs" },
-    globals: { ...globals.node },
+    files: ['**/*.js'],
+    languageOptions: {
+      sourceType: 'commonjs',
+      globals: { ...globals.node },
+      ecmaVersion: 'latest',
+    },
     plugins: {
-      "@stylistic/js": stylisticJs,
+      '@stylistic/js': stylisticJs,
     },
     rules: {
       '@stylistic/js/indent': ['error', 4],
       '@stylistic/js/linebreak-style': ['error', 'unix'],
       '@stylistic/js/quotes': ['error', 'single'],
       '@stylistic/js/semi': ['error', 'never'],
+      eqeqeq: 'error',
+      'no-trailing-spaces': 'error',
+      'object-curly-spacing': ['error', 'always'],
+      'arrow-spacing': ['error', { before: true, after: true }],
     },
   },
-  { files: ["**/*.{js,mjs,cjs}"], languageOptions: { globals: globals.browser } },
+  {
+    ignores: ['dist/**'],
+  },
+  // { files: ['**/*.{js,mjs,cjs}'],
+  //   languageOptions: {
+  //     globals: globals.browser
+  //   }
+  // },
 ]);
