2f65bf16b29e54db8cf65dbb4e2fb9469ca569ad
RealED-io <ed.realuyo@gmail.com>
2025-05-18 03:20:14 +0800

    added logout

--- part5/bloglist-frontend/src/App.jsx
+++ part5/bloglist-frontend/src/App.jsx
@@ -8,19 +8,34 @@ const App = () => {
   const [user, setUser] = useState(null)
 
   useEffect(() => {
+    if (window.localStorage.user) {
+      setUser(JSON.parse(window.localStorage.user))
+    }
+  }, [])
+
+  useEffect(() => {
     blogService.getAll().then(blogs =>
       setBlogs( blogs )
     )  
   }, [])
 
-  console.log(user)
+  const handleLogout = () => {
+    setUser(null)
+    window.localStorage.removeItem('user')
+  }
+
+  const handleLogin = (user) => {
+    setUser(user)
+    window.localStorage.setItem('user', JSON.stringify(user))
+  }
 
   return (
     <div>
       {!user ?
-        <LoginForm onLogin={user => setUser(user)}/> :
+        <LoginForm onLogin={handleLogin} /> :
         <>
           <h2>blogs</h2>
+          <p>{user.name} logged in<button onClick={handleLogout}>logout</button></p>
           {blogs.map(blog =>
               <Blog key={blog.id} blog={blog} />
           )}
