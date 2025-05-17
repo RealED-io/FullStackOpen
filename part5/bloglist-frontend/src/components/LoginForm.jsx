f83f5657befe78d2713d1b738e097521b2c6d105
RealED-io <ed.realuyo@gmail.com>
2025-05-18 02:55:34 +0800

    added login form

---
+++ part5/bloglist-frontend/src/components/LoginForm.jsx
@@ -0,0 +1,35 @@
+import axios from 'axios'
+import loginService from '../services/login'
+
+const LoginForm = () => {
+    const handleLogin = async (event) => {
+        event.preventDefault()
+        const username = event.target.username.value
+        const password = event.target.password.value
+
+        try {
+            const response = await loginService.login({username, password})
+            console.log(response)
+        } catch (error) {
+            console.log(error.message)
+        }
+    }
+    return(
+        <div>
+            <h2>log in to application</h2>
+            <form  onSubmit={handleLogin}>
+                <div>
+                    <label htmlFor="username">username</label>
+                    <input id="username" type="text" />
+                </div>
+                <div>
+                    <label htmlFor="password">password</label>
+                    <input id="password" type="password" />
+                </div>
+                <button type="submit">login</button>
+            </form>
+        </div>
+    )
+}
+
+export default LoginForm
